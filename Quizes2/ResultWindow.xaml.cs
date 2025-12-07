bf372651e8b2a7004460eb9209c2d11112e29ac7
ArtyomSivtsov <artemsivtsov0211@gmail.com>
2025-12-07 14:26:33 +0300

    Добавьте файлы проекта.

---
+++ Quizes2/ResultWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Quizes2
+{
+    public partial class ResultWindow : Window
+    {
+        public ResultWindow(string title, string poz, int score, int maxscore, string result)
+        {
+            InitializeComponent();
+
+            TitleText.Text = "Спасибо за прохождение теста!";
+            //ScoreText.Text = $"Баллы: {score}";
+            //ResultText.Text = result;
+
+            // передаём значения в куб
+            Cube3.Text1 = title;
+            Cube3.ScoreText = $"Баллы: {score} из {maxscore}";
+            Cube3.ResultText = result;
+            Cube3.ImageSide = new BitmapImage(new Uri("Assets/123.png", UriKind.Relative));
+
+            Cube3.CreateCube();
+        }
+    }
+}
